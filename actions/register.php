6966c6d22dd2b387d7dacafc586f1190e1de3872
kafuibedzrah254 <kafuibedzrah254@gmail.com>
2024-04-04 19:23:24 +0000

    secondcommit

---
+++ actions/register.php
@@ -0,0 +1,39 @@
+<?php
+include('connect.php');
+
+$username= $_POST['username'];
+$mobile= $_POST['mobile'];
+$pasword= $_POST['password'];
+$cpassword= $_POST['cpassword'];
+$image= $_FILES['photo']['name'];
+$tmp_name= $_FILES['photo']['tmp_name'];
+$std= $_POST['std'];
+
+
+
+if($pasword!=$cpassword){
+    echo '<script>
+    alert ("Passwords do not match");
+    window.location = "../partials/registration.php";
+    </script>';
+}
+
+else{
+    move_uploaded_file($tmp_name,"../uploads/$image");
+    $sql = "insert into `userdata` (username, mobile, password, photo,standard, status, votes)  values ('$username','$mobile','$password','$image','$std',0,0)" ;
+
+    $result =mysqli_query($con,$sql);
+
+    if($result){
+        echo '<script>
+        alert ("Registration succesful");
+        window.location = "../";
+        </script>';
+    
+
+    }
+}
+
+
+
+?>
